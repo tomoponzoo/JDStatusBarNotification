c846b187940978d9a5601a4a0b57116e2a4afeed
tomoponzoo <tomoponzoo@gmail.com>
2016-02-22 21:11:42 +0900

    Support carthage

---
+++ JDStatusBarNotification/_JDStatusBarNotification.h
@@ -0,0 +1,21 @@
+//
+//  JDStatusBarNotification.h
+//  JDStatusBarNotification
+//
+//  Created by tomoki.koga@bizreach.co.jp on 2016/02/22.
+//  Copyright © 2016年 tomoponzoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JDStatusBarNotification.
+FOUNDATION_EXPORT double JDStatusBarNotificationVersionNumber;
+
+//! Project version string for JDStatusBarNotification.
+FOUNDATION_EXPORT const unsigned char JDStatusBarNotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDStatusBarNotification/PublicHeader.h>
+
+#import <JDStatusBarNotification/JDStatusBarNotification.h>
+#import <JDStatusBarNotification/JDStatusBarStyle.h>
+#import <JDStatusBarNotification/JDStatusBarView.h>
